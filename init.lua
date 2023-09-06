202200d54cd81b38e6ea8b67def937e63e127ff4
Nicholas Sorokin <nicholas.sorokin@eborsystems.com>
2023-09-06 09:56:52 +0930

    add some custom config

--- init.lua
+++ init.lua
@@ -1,21 +1,28 @@
-require "core"
+if vim.g.vscode then
+  vim.opt.runtimepath:remove(vim.fn.expand('~/.config/nvim'))
+  vim.opt.runtimepath:append(vim.fn.expand('~/.config/nvim_vscode'))
 
-local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+  vim.cmd.source(vim.fn.stdpath("config") .. '/../nvim_vscode/init.vim')
+else
+  require "core"
 
-if custom_init_path then
-  dofile(custom_init_path)
+  local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+  if custom_init_path then
+    dofile(custom_init_path)
+  end
+
+  require("core.utils").load_mappings()
+
+  local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+  -- bootstrap lazy.nvim!
+  if not vim.loop.fs_stat(lazypath) then
+    require("core.bootstrap").gen_chadrc_template()
+    require("core.bootstrap").lazy(lazypath)
+  end
+
+  dofile(vim.g.base46_cache .. "defaults")
+  vim.opt.rtp:prepend(lazypath)
+  require "plugins"
 end
-
-require("core.utils").load_mappings()
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
--- bootstrap lazy.nvim!
-if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
-end
-
-dofile(vim.g.base46_cache .. "defaults")
-vim.opt.rtp:prepend(lazypath)
-require "plugins"
